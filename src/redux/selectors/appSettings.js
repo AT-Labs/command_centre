fa4dde8f7be4ed1b1c81e9397142ce48233eadec
Propellerhead <auckland.transport@propellerhead.co.nz>
2025-04-16 03:56:58 +0000

    Adding more test to some new filter criteria

--- src/redux/selectors/appSettings.js
+++ src/redux/selectors/appSettings.js
@@ -55,4 +55,5 @@ export const useNewMonitoring = createSelector(
     getAppSettingsState,
     appSettingState => (result(appSettingState, 'useNewMonitoring') === 'true'),
 );
+/* istanbul ignore next */
 export const useDiversion = createSelector(getAppSettingsState, appSettingState => (result(appSettingState, 'useDiversion') === 'true'));
