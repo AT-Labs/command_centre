a9c48a1fbcf587c547878f6a2882a1178e90f5c1
Propellerhead <auckland.transport@propellerhead.co.nz>
2025-06-12 06:57:47 +0000

    updated added AffectedEntities tests

--- src/components/Control/DisruptionsView/AffectedEntities.spec.jsx
+++ src/components/Control/DisruptionsView/AffectedEntities.spec.jsx
@@ -3,6 +3,7 @@ import React, { useRef } from 'react';
 import { keyBy } from 'lodash-es';
 import { Button } from 'reactstrap';
 import sinon from 'sinon';
+import '@testing-library/jest-dom';
 import { AffectedEntities } from './AffectedEntities';
 
 let sandbox;
@@ -84,5 +85,112 @@ describe('<AffectedEntities />', () => {
         const stopgroupDiv = wrapper.find('ul li').at(2).find('div');
         expect(stopgroupDiv.at(0).text()).toEqual('Stop Group - stop group 1');
         expect(stopgroupDiv.at(1).text()).toEqual('Stop 2, 3');
+    });
+});
+
+describe('View & edit diversions', () => {
+    it('should display an amount of 3 diversions when 3 diversions exist', async () => {
+        wrapper = setup({
+            affectedEntities: [
+                { routeId: 'route1', routeShortName: '1', type: 'route' },
+                { stopId: 'stop1', stopCode: '1', text: '1', type: 'stop' },
+            ],
+            diversions: [
+                { diversionId: 1, diversionRouteVariants: [], routeId: 'route1' },
+                { diversionId: 2, diversionRouteVariants: [], routeId: 'route1' },
+                { diversionId: 3, diversionRouteVariants: [], routeId: 'route1' },
+            ],
+            startTime: '2022-08-03T23:32:00.000Z',
+            endTime: '2022-09-03T23:42:00.000Z',
+            useDiversion: true,
+        });
+
+        const button = wrapper.find('#view-and-edit-diversions-btn');
+        const buttonContent = button.dive();
+        expect(button.exists()).toBeTruthy();
+        expect(buttonContent.text()).toContain('View & edit diversions (3)');
+    });
+
+    it('should display an amount of 1 diversion when diversion list is 1', async () => {
+        wrapper = setup({
+            affectedEntities: [
+                { routeId: 'route1', routeShortName: '1', type: 'route' },
+                { stopId: 'stop1', stopCode: '1', text: '1', type: 'stop' },
+            ],
+            diversions: [
+                { diversionId: 1, diversionRouteVariants: [], routeId: 'route1' },
+            ],
+            startTime: '2022-08-03T23:32:00.000Z',
+            endTime: '2022-09-03T23:42:00.000Z',
+            useDiversion: true,
+        });
+
+        const button = wrapper.find('#view-and-edit-diversions-btn');
+        const buttonContent = button.dive();
+        expect(button.exists()).toBeTruthy();
+        expect(buttonContent.text()).toContain('View & edit diversions (1)');
+    });
+
+    it('should display an amount of 0 diversions when no diversions exist', async () => {
+        wrapper = setup({
+            affectedEntities: [
+                { routeId: 'route1', routeShortName: '1', type: 'route' },
+                { stopId: 'stop1', stopCode: '1', text: '1', type: 'stop' },
+            ],
+            diversions: [],
+            startTime: '2022-08-03T23:32:00.000Z',
+            endTime: '2022-09-03T23:42:00.000Z',
+            useDiversion: true,
+        });
+
+        const button = wrapper.find('#view-and-edit-diversions-btn');
+        const buttonContent = button.dive();
+        expect(button.exists()).toBeTruthy();
+        expect(buttonContent.text()).toContain('View & edit diversions (0)');
+    });
+
+    it('should not render the diversions button when useDiversion is false', () => {
+        wrapper = setup({
+            affectedEntities: [
+                { routeId: 'route1', routeShortName: '1', type: 'route' },
+                { stopId: 'stop1', stopCode: '1', text: '1', type: 'stop' },
+            ],
+            diversions: [
+                { diversionId: 1, diversionRouteVariants: [], routeId: 'route1' },
+                { diversionId: 2, diversionRouteVariants: [], routeId: 'route1' },
+                { diversionId: 3, diversionRouteVariants: [], routeId: 'route1' },
+            ],
+            startTime: '2022-08-03T23:32:00.000Z',
+            endTime: '2022-09-03T23:42:00.000Z',
+            useDiversion: false,
+            viewDiversionsAction: jest.fn(),
+        });
+
+        const button = wrapper.find('#view-and-edit-diversions-btn');
+        expect(button.exists()).toBeFalsy();
+    });
+
+    it('should call viewDiversionsAction when the button is clicked', () => {
+        const viewDiversionsAction = jest.fn();
+        wrapper = setup({
+            affectedEntities: [
+                { routeId: 'route1', routeShortName: '1', type: 'route' },
+                { stopId: 'stop1', stopCode: '1', text: '1', type: 'stop' },
+            ],
+            diversions: [
+                { diversionId: 1, diversionRouteVariants: [], routeId: 'route1' },
+                { diversionId: 2, diversionRouteVariants: [], routeId: 'route1' },
+                { diversionId: 3, diversionRouteVariants: [], routeId: 'route1' },
+            ],
+            startTime: '2022-08-03T23:32:00.000Z',
+            endTime: '2022-09-03T23:42:00.000Z',
+            useDiversion: true,
+            viewDiversionsAction,
+        });
+
+        const button = wrapper.find('#view-and-edit-diversions-btn');
+        expect(button.exists()).toBeTruthy();
+        button.simulate('click');
+        expect(viewDiversionsAction).toHaveBeenCalled();
     });
 });
