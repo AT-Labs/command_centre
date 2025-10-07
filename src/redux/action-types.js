2080d014339d7dae69c58eb9eee56b1f819fd69e
Propellerhead <auckland.transport@propellerhead.co.nz>
2025-10-07 22:42:22 +0000

    Test cleanups

--- src/redux/action-types.js
+++ src/redux/action-types.js
@@ -363,7 +363,4 @@ export default {
     FETCH_CONTROL_BUS_PRIORITY_THRESHOLDS: 'fetch-control-bus-priority-thresholds',
     UPDATE_CONTROL_BUS_PRIORITY_THRESHOLDS_DATAGRID_CONFIG: 'update-control-bus-priority-thresholds-datagrid-config',
     UPDATE_CONTROL_BUS_PRIORITY_PERMISSIONS: 'update-control-bus-priority-permissions',
-    // Detail panel control
-    SET_DETAIL_PANEL_OPEN_FLAG: 'set-detail-panel-open-flag',
-    SET_DISRUPTION_DETAIL_PANEL_OPEN_FLAG: 'set-disruption-detail-panel-open-flag',
 };
