403dd831a22ca42d98b964178f0ffb1e93220f0a
Propellerhead <auckland.transport@propellerhead.co.nz>
2025-10-21 00:16:31 +0000

    Merge branch 'master' of https://bitbucket.org/Propellerhead/at_command_centre into feature/ADO-313666-Limit-the-maximum-number-of-stops-and-routes

--- src/components/Common/Map/RouteShapeEditor/constants.js
+++ src/components/Common/Map/RouteShapeEditor/constants.js
@@ -1,9 +1,8 @@
 // Settings for RouteShapeEditor
-export const ROUTE_SHAPE_OPACITY = 0.8;
+export const ROUTE_SHAPE_OPACITY = 0.6;
 export const ROUTE_SHAPE_WEIGHT = 5;
 export const ROUTE_SHAPE_COLOR = 'DEEPSKYBLUE';
 export const DIVERSION_SHAPE_OPACITY = 0.8;
-export const DIVERSION_SHAPE_WEIGHT = 6;
+export const DIVERSION_SHAPE_WEIGHT = 8;
 export const DIVERSION_SHAPE_COLOR = 'RED';
 export const DEFAULT_AUCKLAND_COORDINATES = [-36.8485, 174.7633];
-export const TOMTOM_STOP_POINTS_LIMIT = 15; // TomTom API allows up to 15 points (including start and end)
